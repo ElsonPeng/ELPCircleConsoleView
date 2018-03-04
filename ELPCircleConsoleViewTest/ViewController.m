6198b71199c992e9b03765e7bc24416b1dd27c67
ElsonPeng <elsonpeng@qq.com>
2018-03-04 20:17:54 +0800

    增加delegate
    delegate

--- ELPCircleConsoleViewTest/ViewController.m
+++ ELPCircleConsoleViewTest/ViewController.m
@@ -20,11 +20,29 @@
     // Do any additional setup after loading the view, typically from a nib.
     self.view.backgroundColor = [UIColor whiteColor];
     ELPCircleConsoleView *gradleView = [[ELPCircleConsoleView alloc]init];
+    gradleView.delegate = self;
     gradleView.frame = CGRectMake(100, 200, 250, 250);
     [self.view addSubview:gradleView];
 
 }
 
+-(void)rotateCircleView:(ELPCircleConsoleView *)rotetaCircleView didRotateWithValueX:(NSString *)Xstr valueY:(NSString *)Ystr stop:(BOOL)needStop moveDirection:(ELPCircleConsoleMoveDirection)orientation{
+    
+    //TODO for your action
+    if(orientation == ELPCircleConsoleMoveDirectionUp){
+        NSLog(@"rotate in up");
+    }
+    else if(orientation == ELPCircleConsoleMoveDirectionDown){
+        NSLog(@"rotate in down");
+    }
+    else if(orientation == ELPCircleConsoleMoveDirectionLeft){
+        NSLog(@"rotate in left");
+    }
+    else if(orientation == ELPCircleConsoleMoveDirectionRight){
+        NSLog(@"rotate in right");
+    }
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
